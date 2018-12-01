bbb7aaa8a1545ce5ff85af2f2c1d3350a3dce869
a <a@kra>
2018-12-01 06:10:49 +0100

    day 1

---
+++ 01.2.go
@@ -0,0 +1,96 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"strings"
+	"strconv"
+)
+
+func must(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+// returns x without the last character
+func nolast(x string) string {
+	return x[:len(x)-1]
+}
+
+// splits a string, trims spaces on every element
+func splitandclean(in, sep string, n int) []string {
+	v := strings.SplitN(in, sep, n)
+	for i := range v {
+		v[i] = strings.TrimSpace(v[i])
+	}
+	return v
+}
+
+// convert string to integer
+func atoi(in string) int {
+	n, err := strconv.Atoi(in)
+	must(err)
+	return n
+}
+
+// convert vector of strings to integer
+func vatoi(in []string) []int {
+	r := make([]int, len(in))
+	for i := range in {
+		var err error
+		r[i], err = strconv.Atoi(in[i])
+		must(err)
+	}
+	return r
+}
+
+func printmatrix(matrix [][]byte) {
+	for i := range matrix {
+		for j := range matrix[i] {
+			fmt.Printf("%c ", matrix[i][j])
+		}
+		fmt.Printf("\n")
+	}
+	fmt.Printf("\n")
+}
+
+func countpixels(matrix [][]byte) (cnt int) {
+	for i := range matrix {
+		for j := range matrix[i] {
+			if matrix[i][j] == '#' {
+				cnt++
+			}
+		}
+	}
+	return cnt
+}
+
+func main() {
+	fmt.Printf("hello\n")
+	buf, err := ioutil.ReadFile("01.txt")
+	must(err)
+	freqs := []int{}
+	for _, line := range strings.Split(string(buf), "\n") {
+		line = strings.TrimSpace(line)
+		if line == "" {
+			continue
+		}
+		freqs = append(freqs, atoi(line))
+	}
+	
+	freq := 0
+	seen := map[int]bool{}
+	i := 0
+	for {
+		if seen[freq] {
+			fmt.Printf("seen again %d\n", freq)
+			break
+		}
+		seen[freq] = true
+		freq += freqs[i % len(freqs)]
+		i++
+	}
+}
+
+// rank 103
