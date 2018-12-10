5d805e63c1d575ede882745a78e71dc3d600ec27
aarzilli <alessandro.arzilli@gmail.com>
2018-12-10 08:58:32 +0100

    update template

--- template.go
+++ template.go
@@ -57,25 +57,67 @@ func vatoiSkip(in []string) []int {
 	return r
 }
 
-func printmatrix(matrix [][]byte) {
-	for i := range matrix {
-		for j := range matrix[i] {
-			fmt.Printf("%c ", matrix[i][j])
-		}
-		fmt.Printf("\n")
-	}
-	fmt.Printf("\n")
+func getints(in string, hasneg bool) []int {
+	v := getnums(in, hasneg, false)
+	return vatoi(v)
 }
 
-func countpixels(matrix [][]byte) (cnt int) {
-	for i := range matrix {
-		for j := range matrix[i] {
-			if matrix[i][j] == '#' {
-				cnt++
+func getfloats(in string, hasneg bool) []float64 {
+	v := getnums(in, hasneg, false)
+	r := make([]float64, len(v))
+	for i := range v {
+		var err error
+		r[i], err = strconv.ParseFloat(v[i], 64)
+		must(err)
+	}
+	return r
+}
+
+func getnums(in string, hasneg, hasdot bool) []string {
+	r := []string{}
+	start := -1
+
+	flush := func(end int) {
+		if start < 0 {
+			return
+		}
+		hasdigit := false
+		for i := start; i < end; i++ {
+			if in[i] >= '0' && in[i] <= '9' {
+				hasdigit = true
+				break
+			}
+		}
+		if hasdigit {
+			r = append(r, in[start:end])
+		}
+		start = -1
+	}
+
+	for i, ch := range in {
+		isnumch := false
+
+		switch {
+		case hasneg && (ch == '-'):
+			isnumch = true
+		case hasdot && (ch == '.'):
+			isnumch = true
+		case ch >= '0' && ch <= '9':
+			isnumch = true
+		}
+
+		if start >= 0 {
+			if !isnumch {
+				flush(i)
+			}
+		} else {
+			if isnumch {
+				start = i
 			}
 		}
 	}
-	return cnt
+	flush(len(in))
+	return r
 }
 
 func main() {
