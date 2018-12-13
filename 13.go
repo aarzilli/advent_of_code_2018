73867c7bf6e0cb6159a1296fb855de46a225e595
aarzilli <alessandro.arzilli@gmail.com>
2018-12-13 07:07:32 +0100

    cleanup

--- 13.go
+++ 13.go
@@ -5,7 +5,6 @@ import (
 	"io/ioutil"
 	"os"
 	"sort"
-	"strconv"
 	"strings"
 )
 
@@ -13,113 +12,6 @@ func must(err error) {
 	if err != nil {
 		panic(err)
 	}
-}
-
-// returns x without the last character
-func nolast(x string) string {
-	return x[:len(x)-1]
-}
-
-// splits a string, trims spaces on every element
-func splitandclean(in, sep string, n int) []string {
-	v := strings.SplitN(in, sep, n)
-	for i := range v {
-		v[i] = strings.TrimSpace(v[i])
-	}
-	return v
-}
-
-// convert string to integer
-func atoi(in string) int {
-	n, err := strconv.Atoi(in)
-	must(err)
-	return n
-}
-
-// convert vector of strings to integer
-func vatoi(in []string) []int {
-	r := make([]int, len(in))
-	for i := range in {
-		var err error
-		r[i], err = strconv.Atoi(in[i])
-		must(err)
-	}
-	return r
-}
-
-// convert vector of strings to integer, discard non-ints
-func vatoiSkip(in []string) []int {
-	r := make([]int, 0, len(in))
-	for i := range in {
-		n, err := strconv.Atoi(in[i])
-		if err == nil {
-			r = append(r, n)
-		}
-	}
-	return r
-}
-
-func getints(in string, hasneg bool) []int {
-	v := getnums(in, hasneg, false)
-	return vatoi(v)
-}
-
-func getfloats(in string, hasneg bool) []float64 {
-	v := getnums(in, hasneg, false)
-	r := make([]float64, len(v))
-	for i := range v {
-		var err error
-		r[i], err = strconv.ParseFloat(v[i], 64)
-		must(err)
-	}
-	return r
-}
-
-func getnums(in string, hasneg, hasdot bool) []string {
-	r := []string{}
-	start := -1
-
-	flush := func(end int) {
-		if start < 0 {
-			return
-		}
-		hasdigit := false
-		for i := start; i < end; i++ {
-			if in[i] >= '0' && in[i] <= '9' {
-				hasdigit = true
-				break
-			}
-		}
-		if hasdigit {
-			r = append(r, in[start:end])
-		}
-		start = -1
-	}
-
-	for i, ch := range in {
-		isnumch := false
-
-		switch {
-		case hasneg && (ch == '-'):
-			isnumch = true
-		case hasdot && (ch == '.'):
-			isnumch = true
-		case ch >= '0' && ch <= '9':
-			isnumch = true
-		}
-
-		if start >= 0 {
-			if !isnumch {
-				flush(i)
-			}
-		} else {
-			if isnumch {
-				start = i
-			}
-		}
-	}
-	flush(len(in))
-	return r
 }
 
 var M [][]byte
@@ -292,7 +184,7 @@ func simstep() {
 }
 
 const part1 = false
-const debugsim = true
+const debugsim = false
 
 func checkcollision() {
 	collided := false
@@ -369,7 +261,7 @@ func main() {
 		}
 		if cnt == 1 {
 			fmt.Printf("last cart is %d at %d,%d\n", idx, carts[idx].pos.j, carts[idx].pos.i)
-			os.Exit(1)
+			return
 		}
 	}
 }
